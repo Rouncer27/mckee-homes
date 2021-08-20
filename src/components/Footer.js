5212bd08871125a2e81d7e01be9cb3da90705653
Rouncer27 <trevor@switchbackcreative.ca>
2021-08-20 12:48:34 -0600

    added the footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,118 @@
 import React from "react"
+import { graphql, useStaticQuery, Link } from "gatsby"
+import styled from "styled-components"
+import { B2White, colors, medWrapper } from "../styles/helpers"
+
+const getData = graphql`
+  {
+    quickLinks: wpMenu(name: { eq: "Quick Links Menu" }) {
+      name
+      menuItems {
+        nodes {
+          label
+          url
+          parentDatabaseId
+          parentId
+          id
+          databaseId
+        }
+      }
+    }
+  }
+`
 
 const Footer = () => {
+  const data = useStaticQuery(getData)
+  const { quickLinks } = data
+  console.log(quickLinks)
   return (
-    <div>
-      <h1>Footer</h1>
-    </div>
+    <FooterStyled>
+      <div className="wrapper">
+        <div className="foot-social"></div>
+        <div className="foot-quick-links">
+          <nav>
+            <ul>
+              {quickLinks.menuItems.nodes.map(item => {
+                const slug = item.url
+                  .split("/")
+                  .filter(item => item !== "")
+                  .join("/")
+                return (
+                  <li key={item.id}>
+                    <Link to={`/${slug}`}>{item.label}</Link>
+                  </li>
+                )
+              })}
+            </ul>
+          </nav>
+        </div>
+        <div className="foot-mail"></div>
+      </div>
+      <div className="foot-bottom">
+        <ul>
+          <li>
+            <Link to="">Privacy</Link>
+          </li>
+          <li>
+            <Link to="">Disclaimer</Link>
+          </li>
+        </ul>
+      </div>
+    </FooterStyled>
   )
 }
+
+const FooterStyled = styled.footer`
+  background-color: ${colors.colorPrimary};
+
+  .wrapper {
+    ${medWrapper};
+  }
+
+  .foot-social {
+    width: calc(15%);
+  }
+
+  .foot-quick-links {
+    width: calc(70%);
+
+    ul {
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        a {
+          ${B2White};
+          text-align: center;
+          text-transform: uppercase;
+        }
+      }
+    }
+  }
+
+  .foot-mail {
+    width: calc(15%);
+  }
+
+  .foot-bottom {
+    width: calc(100%);
+    padding: 1rem 4rem;
+    background-color: #42454a;
+
+    ul {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+
+      li {
+        a {
+          ${B2White};
+          text-align: center;
+          text-transform: uppercase;
+        }
+      }
+    }
+  }
+`
 
 export default Footer
