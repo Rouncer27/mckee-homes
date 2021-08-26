624940bb9efc6c530d30218d72ea28eeff912e3c
Rouncer27 <trevor@switchbackcreative.ca>
2021-08-26 10:21:46 -0600

    added the wysiwyg section component

--- src/components/PageComponents/WysiwygButton.js
+++ src/components/PageComponents/WysiwygButton.js
@@ -1,11 +1,72 @@
+import { Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
+import {
+  Btn1Grey,
+  H1Navy,
+  H2Black,
+  H3Black,
+  medWrapper,
+  standardWrapper,
+} from "../../styles/helpers"
+import Wysiwyg from "./Wysiwyg"
 
-const WysiwygButton = () => {
+const WysiwygButton = ({ data }) => {
+  console.log("WysiwygButton: ", data)
   return (
-    <div>
-      <h2>Wysiwyg Button</h2>
-    </div>
+    <SectionStyled>
+      <div className="wrapper">
+        <div className="title">
+          <h2>{data.title}</h2>
+        </div>
+        <div className="side-title">
+          <h3>{data.sideTitle}</h3>
+        </div>
+        <div className="content">
+          <Wysiwyg data={{ wysiwyg: data.content }} />
+        </div>
+        <div className="button">
+          <Link to={`/${data.buttonSlug}`}>{data.buttonText}</Link>
+        </div>
+      </div>
+    </SectionStyled>
   )
 }
+
+const SectionStyled = styled.section`
+  .wrapper {
+    ${standardWrapper};
+    position: relative;
+  }
+
+  .title {
+    width: 100%;
+
+    h2 {
+      ${H2Black};
+    }
+  }
+
+  .side-title {
+    position: absolute;
+    top: 20rem;
+    left: -5rem;
+    transform-origin: center left;
+    transform: rotate(-90deg);
+
+    h3 {
+      ${H3Black};
+      text-transform: uppercase;
+    }
+  }
+
+  .button {
+    width: 100%;
+
+    a {
+      ${Btn1Grey};
+    }
+  }
+`
 
 export default WysiwygButton
