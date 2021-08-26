4820bb07a6ac3ea097a8f1839dfecc259c30c27b
Rouncer27 <trevor@switchbackcreative.ca>
2021-08-26 10:51:42 -0600

    logo compoent

--- src/components/PageComponents/Logos.js
+++ src/components/PageComponents/Logos.js
@@ -1,12 +1,64 @@
+import { Link } from "gatsby"
 import React from "react"
+import styled from "styled-components"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { medWrapper } from "../../styles/helpers"
 
 const Logos = ({ data }) => {
   console.log("Logos", data)
   return (
-    <div>
-      <h2>Logos</h2>
-    </div>
+    <SectionStyled>
+      <div className="wrapper">
+        {data.logos.map((logo, index) => {
+          const imageDisplay = getImage(
+            logo.logo.localFile.childImageSharp.gatsbyImageData
+          )
+          const imageAlt = logo.logo.altText
+
+          if (logo.url) {
+            return (
+              <LogoLink to={`/${logo.url}`} key={index}>
+                <GatsbyImage
+                  image={imageDisplay}
+                  alt={imageAlt}
+                  layout="fullWidth"
+                  formats={["auto", "webp", "avif"]}
+                />
+              </LogoLink>
+            )
+          } else {
+            ;<Logo to={`/${logo.url}`} key={index}>
+              <GatsbyImage
+                image={imageDisplay}
+                alt={imageAlt}
+                layout="fullWidth"
+                formats={["auto", "webp", "avif"]}
+              />
+            </Logo>
+          }
+        })}
+      </div>
+    </SectionStyled>
   )
 }
+
+const SectionStyled = styled.div`
+  margin: 2rem auto;
+
+  .wrapper {
+    ${medWrapper};
+    align-items: center;
+  }
+`
+
+const LogoLink = styled(Link)`
+  width: calc(100% / 5);
+  padding: 1rem;
+`
+
+const Logo = styled.div`
+  width: calc(100% / 5);
+  padding: 1rem;
+`
 
 export default Logos
