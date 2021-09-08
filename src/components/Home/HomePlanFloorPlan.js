11ce2d2c8f2ac00386a6fbd7871a213741c20634
Rouncer27 <trevor@switchbackcreative.ca>
2021-09-08 08:47:32 -0600

    like and download buttons

--- src/components/Home/HomePlanFloorPlan.js
+++ src/components/Home/HomePlanFloorPlan.js
@@ -1,7 +1,14 @@
 import React from "react"
 import styled from "styled-components"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
-import { B2Black, colors, standardWrapper } from "../../styles/helpers"
+import {
+  B2Black,
+  B2Grey,
+  Btn1Grey,
+  colors,
+  H1Navy,
+  standardWrapper,
+} from "../../styles/helpers"
 
 const HomePlanFloorPlan = ({ home }) => {
   const floorImg = getImage(
@@ -27,6 +34,22 @@ const HomePlanFloorPlan = ({ home }) => {
               />
             </div>
           </div>
+          <div className="floorplan-wrapper__like">
+            <a
+              className="floorplan-wrapper__like--download"
+              target="_blank"
+              rel="noreferrer"
+              href={home.acfHomePlans.floorPlanPdf.localFile.publicURL}
+            >
+              Download Floor Plan
+            </a>
+            <button
+              className="floorplan-wrapper__like--save"
+              onClick={() => console.log("LIKE!!")}
+            >
+              Save Floor Plan
+            </button>
+          </div>
         </div>
       </div>
     </SectionStyled>
@@ -48,6 +71,33 @@ const SectionStyled = styled.section`
 
     &__inner {
       ${standardWrapper};
+    }
+
+    &__plan {
+      p {
+        ${H1Navy};
+        margin-top: 2rem;
+        margin-bottom: 2rem;
+      }
+    }
+
+    &__like {
+      width: 100%;
+      margin-top: 3rem;
+      padding-top: 2.5rem;
+      border-top: solid 0.25rem ${colors.colorTertiary};
+
+      &--download {
+        ${Btn1Grey};
+        margin-right: 2.5rem;
+      }
+
+      &--save {
+        ${B2Grey};
+        border: none;
+        background-color: transparent;
+        text-transform: uppercase;
+      }
     }
   }
 `
