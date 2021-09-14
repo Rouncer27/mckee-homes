1d26c56508c256e773c304b22d8fe6a0f58442fa
Rouncer27 <trevor@switchbackcreative.ca>
2021-09-14 16:42:45 -0600

    added the map and contact form sections to the contact page

--- src/components/PageComponents/Map.js
+++ src/components/PageComponents/Map.js
@@ -1,11 +1,50 @@
 import React from "react"
+import styled from "styled-components"
+import { B1Black, H1Navy } from "../../styles/helpers"
 
-const Map = () => {
+const Map = ({ data }) => {
+  console.log(data)
   return (
-    <div>
-      <h1>Map</h1>
-    </div>
+    <StyledSection>
+      <div className="wrapper">
+        <div className="content">
+          <h2>{data.title}</h2>
+          <div dangerouslySetInnerHTML={{ __html: data.content }} />
+        </div>
+        <div className="map">
+          <div dangerouslySetInnerHTML={{ __html: data.mapEmbedCode }} />
+        </div>
+      </div>
+    </StyledSection>
   )
 }
+
+const StyledSection = styled.section`
+  margin: 2.5rem auto;
+
+  .wrapper {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+  }
+
+  .content {
+    width: calc(50% - 6rem);
+    margin-right: 6rem;
+    text-align: right;
+
+    h2 {
+      ${H1Navy};
+    }
+
+    p {
+      ${B1Black};
+    }
+  }
+
+  .map {
+    width: calc(50%);
+  }
+`
 
 export default Map
