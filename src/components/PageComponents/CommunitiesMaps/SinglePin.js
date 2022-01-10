018b2046a770efbac26acaa75d8a9690938596bd
Rouncer27 <trevor@switchbackcreative.ca>
2022-01-10 11:43:40 -0700

    added box shadow

--- src/components/PageComponents/CommunitiesMaps/SinglePin.js
+++ src/components/PageComponents/CommunitiesMaps/SinglePin.js
@@ -1,18 +1,11 @@
 import React from "react"
-import { graphql, Link, useStaticQuery } from "gatsby"
-import { GatsbyImage, getImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 import styled from "styled-components"
 
 import { B2Black, Btn1Grey, colors } from "../../../styles/helpers"
 
 const SinglePin = ({ imgSrc, alt, title, details, slug, classmodifier }) => {
-  const addActiveClass = e => {
-    e.currentTarget.classList.add("active-pin")
-  }
-
-  const removeActiveClass = e => {
-    e.currentTarget.classList.remove("active-pin")
-  }
   return (
     <DivStyled
       className={`pin ${classmodifier}`}
@@ -54,7 +47,6 @@ const DivStyled = styled.div`
   display: block;
   position: absolute;
   width: 12vw;
-
   cursor: pointer;
   z-index: 500;
 
@@ -69,6 +61,7 @@ const DivStyled = styled.div`
     width: 25.5rem;
     padding: 1rem 1.5rem;
     background-color: ${colors.white};
+    box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
     transition: all 0.3s ease-out;
     opacity: 0;
     visibility: hidden;
@@ -118,21 +111,38 @@ const DivStyled = styled.div`
   &.pins__cooperscrossing {
     left: 45vw;
     bottom: 5%;
+
+    .pin__details {
+      top: -27.5rem;
+    }
   }
 
   &.pins__kingsHeights {
     right: 22.5vw;
     bottom: 27.5%;
+
+    .pin__details {
+      left: -27.5rem;
+    }
   }
 
   &.pins__lanarklanding {
     right: 22.5vw;
     bottom: 5%;
+
+    .pin__details {
+      top: -27.5rem;
+      left: -27.5rem;
+    }
   }
 
   &.pins__ravenswood {
     right: 10vw;
     bottom: 20%;
+
+    .pin__details {
+      left: -27.5rem;
+    }
   }
 `
 
