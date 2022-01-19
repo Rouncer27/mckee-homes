2ee4380988d40c31d0d4ec59e11bbd75981c27a6
Rouncer27 <trevor@switchbackcreative.ca>
2022-01-19 11:34:53 -0700

    added the ability to remove the homes plans no longer availalbe

--- src/components/AppRoutes/AppComponents/NoteCardNot.js
+++ src/components/AppRoutes/AppComponents/NoteCardNot.js
@@ -1,13 +1,31 @@
-import React from "react"
+import React, { useContext } from "react"
 import styled from "styled-components"
-import { B1Black, H3Navy } from "../../../styles/helpers"
+import { B1Black, H3Navy, Btn1Danger } from "../../../styles/helpers"
+import { UserContext } from "../../../context/UserContext"
+import { AlertContext } from "../../../context/AlertContext"
 
-const NoteCardNot = ({ plan }) => {
+import deletePlan from "../AppActions/deletePlan"
+
+const NoteCardNot = ({ plan, url }) => {
+  const [, userDispatch] = useContext(UserContext)
+  const [, alertDispatch] = useContext(AlertContext)
+
+  const handleOnDelete = () => {
+    deletePlan(userDispatch, alertDispatch, plan.id, url)
+  }
+
   return (
     <HomeCard key={plan.id}>
       <p className="title">
         {plan.title} <span>No longer available</span>
       </p>
+      <div className="notes">
+        <div className="notes__actions">
+          <button className="notes__actions--delete" onClick={handleOnDelete}>
+            Remove
+          </button>
+        </div>
+      </div>
     </HomeCard>
   )
 }
@@ -33,6 +51,17 @@ const HomeCard = styled.div`
 
     &__title {
       ${B1Black};
+    }
+
+    &__actions {
+      width: 100%;
+      margin-top: 2.5rem;
+      margin-bottom: 2.5rem;
+
+      &--delete {
+        ${Btn1Danger};
+        margin-left: 1rem;
+      }
     }
   }
 `
