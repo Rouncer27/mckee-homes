404a8755976d1aeba45e1f6553aab7e9b89f37bc
Rouncer27 <trevor@switchbackcreative.ca>
2022-04-14 13:49:37 -0600

    seo fixes

--- src/templates/quickPossession.js
+++ src/templates/quickPossession.js
@@ -2,7 +2,7 @@ import React from "react"
 import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
-import Seo from "../components/SEO"
+import Seo from "../components/Seo"
 import QuickPossession from "../components/Home/QuickPossession"
 
 const quickPossession = props => {
