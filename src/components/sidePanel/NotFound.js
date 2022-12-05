88b59b259bb9270a69979c4f2ff67f9064591b75
Rouncer27 <trevor@switchbackcreative.ca>
2022-12-05 09:30:39 -0700

    added side panel styles

---
+++ src/components/sidePanel/NotFound.js
@@ -0,0 +1,31 @@
+import React from "react"
+import styled from "styled-components"
+import { B1Black } from "../../styles/helpers"
+
+const NotFound = () => {
+  return (
+    <NotFoundDiv>
+      <div className="wrapper">
+        <p>
+          Sorry, there was no data found on this lot. Please pick another lot,
+          thank you.
+        </p>
+      </div>
+    </NotFoundDiv>
+  )
+}
+
+const NotFoundDiv = styled.div`
+  width: 100%;
+
+  .wrapper {
+    max-width: 40rem;
+    margin: 2rem auto;
+  }
+
+  p {
+    ${B1Black};
+  }
+`
+
+export default NotFound
