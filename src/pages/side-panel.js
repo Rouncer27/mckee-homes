01657b04221a69bfce3aed85dbaf302da129e268
Rouncer27 <trevor@switchbackcreative.ca>
2023-10-16 14:42:11 -0600

    added width icon

--- src/pages/side-panel.js
+++ src/pages/side-panel.js
@@ -79,9 +79,30 @@ const SidePanel = props => {
       ) {
         return true
       } else if (
+        lotworks.community === "Lewiston" &&
+        home.node.communities.nodes.find(
+          community => community.slug === "lewiston"
+        )
+      ) {
+        return true
+      } else if (
+        lotworks.community === "Goldwyn" &&
+        home.node.communities.nodes.find(
+          community => community.slug === "goldwyn"
+        )
+      ) {
+        return true
+      } else if (
         lotworks.community === "Vista Crossing" &&
         home.node.communities.nodes.find(
           community => community.slug === "vista-crossing"
+        )
+      ) {
+        return true
+      } else if (
+        lotworks.community === "Mandalay Estates" &&
+        home.node.communities.nodes.find(
+          community => community.slug === "mandalay-estates"
         )
       ) {
         return true
