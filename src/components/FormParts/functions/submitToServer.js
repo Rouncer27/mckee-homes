08dea926cc3bb6326bb8a830bdc8b9be8f04c18f
Trevor Rounce <trevor@switchbackcreative.ca>
2024-11-08 10:50:31 -0700

    removed the master thank you send

--- src/components/FormParts/functions/submitToServer.js
+++ src/components/FormParts/functions/submitToServer.js
@@ -5,16 +5,17 @@ const submitToWebServer = async (formID, data) => {
   const config = { headers: { "Content-Type": "multipart/form-data" } }
   const serverResponse = await axios.post(FORM_POST_URL, data, config)
 
-  if (serverResponse.data.status === "mail_sent" && formID !== 11435) {
-    const serverTwoResponse = await axios.post(
-      `https://mckeehomes.swbdatabases.ca/wp-json/contact-form-7/v1/contact-forms/3536/feedback`,
-      data,
-      config
-    )
-    if (serverTwoResponse === "mail_sent") {
-      return { errors: false, errorMessages: [] }
+  if (serverResponse.data.status === "mail_sent") {
+    if (formID !== 11435) {
+      const serverTwoResponse = await axios.post(
+        `https://mckeehomes.swbdatabases.ca/wp-json/contact-form-7/v1/contact-forms/3536/feedback`,
+        data,
+        config
+      )
+      if (serverTwoResponse === "mail_sent") {
+        return { errors: false, errorMessages: [] }
+      }
     }
-
     return { errors: false, errorMessages: [] }
   } else {
     return { errors: true, errorMessages: serverResponse.data.invalid_fields }
