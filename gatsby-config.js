959bc55193f0411dda22c69da4a30c7ee2df1f90
Trevor Rounce <trevor@switchbackcreative.ca>
2025-08-14 10:06:33 -0600

    fixed headers

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         headers: {
           "/*": [
             "cache-control: public,max-age=60",
-            "X-Frame-Options: sameorigin",
+            "X-Frame-Options: SAMEORIGIN",
             "X-XSS-Protection: 1; mode=block",
             "X-Content-Type-Options: nosniff",
             "Feature-Policy: camera 'none'; geolocation 'none'; microphone 'none'",
